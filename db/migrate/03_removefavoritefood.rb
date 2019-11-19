3c6362f4d1b647bf92b08f24cbb41c0ee0103324
joshua richards <jermain119@gmail.com>
2019-11-19 20:43:26 +0000

    Automatically backed up by Learn

--- db/migrate/03_removefavoritefood.rb
+++ db/migrate/03_removefavoritefood.rb
@@ -1,5 +1,5 @@
 class RemoveFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
   def change
-    add_column :artists, :favorite_food, :string
+    remove_column :artists, :favorite_food
   end
 end
